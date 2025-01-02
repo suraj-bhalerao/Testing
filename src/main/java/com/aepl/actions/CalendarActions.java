73aebe2a148a3e078084a819b436514d11bdfd3f
surajB9204 <surajbhalerao2024@gmail.com>
2025-01-02 20:52:53 +0530

    Suraj:Edit Drag and Drop class add doc

--- src/main/java/com/aepl/actions/CalendarActions.java
+++ src/main/java/com/aepl/actions/CalendarActions.java
@@ -1,8 +1,9 @@
 package com.aepl.actions;
-import java.time.Duration;
 
 import java.time.Duration;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,23 +14,22 @@ public class CalendarActions {
 	private WebDriver driver;
 	private WebDriverWait wait;
 
+	private static final Logger logger = LogManager.getLogger(CalendarActions.class);
+
 	public CalendarActions(WebDriver driver) {
 		this.driver = driver;
 		this.wait = new WebDriverWait(this.driver, Duration.ofSeconds(10));
 	}
-	
+
 	public void selectDate(By calendarLocator, String targetDate) {
-		// Open the calendar widget
 		WebElement calendarElement = wait.until(ExpectedConditions.elementToBeClickable(calendarLocator));
 		calendarElement.click();
 
-		// Parse the date into day, month, and year
 		String[] dateParts = targetDate.split("-");
 		String day = dateParts[0];
 		String month = dateParts[1];
 		String year = dateParts[2];
 
-		// Navigate to the correct year and month
 		while (true) {
 			String displayedMonthYear = driver.findElement(By.cssSelector("selector-for-month-year")).getText();
 			if (displayedMonthYear.contains(month) && displayedMonthYear.contains(year)) {
@@ -38,13 +38,8 @@ public class CalendarActions {
 			driver.findElement(By.cssSelector("selector-for-next-month")).click(); // Navigate to the next month
 		}
 
-		// Select the day
 		By dayLocator = By.xpath("//td[text()='" + day + "']");
 		WebElement dayElement = wait.until(ExpectedConditions.elementToBeClickable(dayLocator));
 		dayElement.click();
-	}
-	public void dj (){
-	System.out.println();
-		
 	}
 }
