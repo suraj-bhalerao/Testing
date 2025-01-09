a77912a105ebd234e187cee6b06d9fba2450a209
surajB9204 <surajbhalerao2024@gmail.com>
2025-01-09 10:57:46 +0530

    Suraj: Dealer FOTA Page

--- src/main/java/com/aepl/pages/DealearFotaPage.java
+++ src/main/java/com/aepl/pages/DealearFotaPage.java
@@ -1,6 +1,9 @@
 package com.aepl.pages;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.log4j.LogManager;
@@ -29,6 +32,7 @@ public class DealearFotaPage {
 	private By addApprovedFileBtn = By.xpath("/html/body/app-root/app-dealer-fota/div/div/div[2]/button");
 	private By fileNameInput = By.xpath("//input[@placeholder='Enter File Name']");
 	private By saveFileButton = By.xpath("//button[@class='btn btn-primary w-100']");
+	private By tableRowsLocator = By.xpath("//tr[@class=\"odd text-center ng-star-inserted\"]");
 	
 	// Methods goes here
 	public void clickNavBar() {
@@ -71,20 +75,54 @@ public class DealearFotaPage {
 	}
 	
 	public void addNewFileAndValidate() {
-		
-		// File Adding 
-		logger.info("Trying to added the new file");
-		WebElement inputBox = wait.until(ExpectedConditions.visibilityOfElementLocated(fileNameInput));
-		WebElement fileBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(saveFileButton));
-		
-		logger.info("Trying to add new file");
-		inputBox.sendKeys(Keys.ENTER);
-		inputBox.sendKeys("Testing File Name");
-		logger.info("Clicking on the file button");
-		fileBtn.click();
-		
-		
-		// Validating upload file 
-		
+	    // File Adding 
+	    logger.info("Trying to add the new file");
+	    WebElement inputBox = wait.until(ExpectedConditions.visibilityOfElementLocated(fileNameInput));
+	    WebElement fileBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(saveFileButton));
+	    
+	    String fileName = "Testing File Name";
+	    logger.info("Entering file name in the input box");
+	    inputBox.sendKeys(Keys.ENTER);
+	    inputBox.sendKeys(fileName);
+	    
+	    // Capture the current date and time in the specified format
+	    String dateTimeFormat = "MM/dd/yyyy, hh:mm:ss a";
+	    SimpleDateFormat formatter = new SimpleDateFormat(dateTimeFormat);
+	    String capturedDateTime = formatter.format(new Date());
+	    
+	    logger.info("Clicking on the save file button");
+	    fileBtn.click();
+	    
+	    // Pause briefly to allow the table to update
+	    try {
+	        Thread.sleep(2000); // Adjust as per your application's update time
+	    } catch (InterruptedException e) {
+	        logger.error("Thread interrupted while waiting for table update", e);
+	    }
+	    
+	    // Validate the uploaded file in the table
+	    logger.info("Validating the uploaded file in the table");
+	    List<WebElement> tableRows = driver.findElements(tableRowsLocator);
+	    boolean isFileValidated = false;
+	    
+	    for (WebElement row : tableRows) {
+	        List<WebElement> cells = row.findElements(By.tagName("td"));
+	        
+	        if (cells.size() > 0) {
+	            String rowFileName = cells.get(0).getText(); 
+	            String rowDateTime = cells.get(1).getText(); 
+	            
+	            if (fileName.equals(rowFileName) && capturedDateTime.equals(rowDateTime)) {
+	                logger.info("File name and date-time validated successfully in the table");
+	                isFileValidated = true;
+	                break;
+	            }
+	        }
+	    }
+	    
+	    if (!isFileValidated) {
+	        logger.error("File validation failed. File name or date-time not found in the table");
+	        throw new RuntimeException("File validation failed");
+	    }
 	}
 }
