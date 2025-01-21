e5b0261be6ed29119d475cecf42f44cc1d02713a
surajB9204 <surajbhalerao2024@gmail.com>
2025-01-21 20:30:33 +0530

    Suraj: new changes commited

--- src/main/java/com/aepl/pages/TicketDashboardPage.java
+++ src/main/java/com/aepl/pages/TicketDashboardPage.java
@@ -1,65 +1,34 @@
 package com.aepl.pages;
 
 import java.time.Duration;
-import java.util.List;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.aepl.util.CommonMethod;
 
 public class TicketDashboardPage {
-    // Global Variable and instances
-    private final WebDriver driver;
-    private final WebDriverWait wait;
-    @SuppressWarnings("unused")
-    private final CommonMethod commmethod;
-    private static final Logger logger = LogManager.getLogger(TicketDashboardPage.class);
+	// Global Variable and instances
+	private final WebDriver driver;
+	private final WebDriverWait wait;
+	@SuppressWarnings("unused")
+	private final CommonMethod commmethod;
+	private static final Logger logger = LogManager.getLogger(TicketDashboardPage.class);
 
-    // Constructor
-    public TicketDashboardPage(WebDriver driver) {
-        super();
-        this.driver = driver;
-        this.commmethod = new CommonMethod(driver);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-    }
+	// Constructor
+	public TicketDashboardPage(WebDriver driver) {
+		super();
+		this.driver = driver;
+		this.commmethod = new CommonMethod(driver);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+	}
 
-    // Locators goes here
-    private final By navBarLink = By.xpath("//*[@id=\"navbarDropdownProfile\"]/span");
-    private final By ticketDash = By.xpath("//a[@class=\"dropdown-item ng-star-inserted\"][3]");
+	// Locators goes here
+	private final By navBarLink = By.xpath("//span[@class=\"headers_custom color_3D5772\"]");
+	private final By ticketDash = By.xpath("//a[@class='dropdown-item ng-star-inserted'][3]");
 
-
-    // Methods goes here
-    public void clickNavBar() {
-        List<WebElement> navBarLinks = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(navBarLink));
-
-        boolean isClicked = false;
-        for (WebElement link : navBarLinks) {
-            if (link.getText().equalsIgnoreCase("Ticket Dashboard")) {
-                link.click();
-                isClicked = true;
-                clickDropDownOption();
-                break;
-            }
-        }
-        if (!isClicked) {
-            throw new RuntimeException("Failed to find and click on 'Ticket Dashboard' in the navigation bar.");
-        }
-    }
-
-    public void clickDropDownOption() {
-        try {
-            WebElement ticketDashboard = wait.until(ExpectedConditions.visibilityOfElementLocated(ticketDash));
-            ticketDashboard.click();
-            driver.getCurrentUrl();
-        } catch (Exception e) {
-            logger.error("Error while clicking on Ticket Dashboard option.", e);
-            throw new RuntimeException("Failed to click on Ticket Dashboard option", e);
-        }
-    }
+	// Methods goes here
 }
--- src/test/java/com/aepl/tests/TicketDashboardPageTest.java
+++ src/test/java/com/aepl/tests/TicketDashboardPageTest.java
@@ -6,7 +6,6 @@ import org.testng.annotations.Test;
 import com.aepl.base.TestBase;
 import com.aepl.pages.LoginPage;
 import com.aepl.pages.TicketDashboardPage;
-import com.aepl.util.CommonMethod;
 import com.aepl.util.ConfigProperties;
 import com.aepl.util.ExcelUtility;
 
@@ -29,28 +28,5 @@ public class TicketDashboardPageTest extends TestBase {
     public void login() {
         loginPage.enterUsername(ConfigProperties.getProperty("valid.username"))
                 .enterPassword(ConfigProperties.getProperty("valid.password")).clickLogin();
-    }
-
-    @Test(priority = 2)
-    public void testClickNavBar() {
-        logger.info("Testing the navigation for the Ticket Dashboard");
-        String testCaseName = "Test Ticket Dashboard";
-        String expectedResult = ConfigProperties.getProperty("ticket.dash");
-        String actualResult = "";
-        String result;
-        try {
-            logger.info("Trying to click on the nav bar links");
-            ticketDashboard.clickNavBar();
-            actualResult = driver.getCurrentUrl();
-            result = expectedResult.equalsIgnoreCase(actualResult) ? "PASS" : "FAIL";
-            logger.info("Result is : " + result);
-            excelUtility.writeTestDataToExcel(testCaseName, expectedResult, actualResult, result);
-        } catch (Exception e) {
-            logger.warn("Error");
-            CommonMethod.captureScreenshot(testCaseName);
-            actualResult = driver.getCurrentUrl();
-            result = expectedResult.equalsIgnoreCase(actualResult) ? "PASS" : "FAIL";
-            excelUtility.writeTestDataToExcel(testCaseName, expectedResult, actualResult, result);
-        }
     }
 }
