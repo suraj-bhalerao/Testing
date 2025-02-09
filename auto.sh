38a48e5b10e8b2558b9cc800767120ba25a5b51d
surajB2024 <surajbhalerao2024@gmail.com>
2025-02-09 18:28:09 +0530

    Push by: surajB2024 | Updated files:

--- auto.sh
+++ auto.sh
@@ -25,6 +25,7 @@ PROJECT_DIR="/d/Testing/testing_updated"
 cd "$PROJECT_DIR" || { echo "Failed to navigate to $PROJECT_DIR"; exit 1; }
 
 # Run Maven clean install
+echo " "
 echo "Cleaning and installing the maven repository"
 mvn clean install
 
@@ -32,16 +33,22 @@ mvn clean install
 sleep 30
 
 # Check which files have been changed
+echo " "
+echo "Checking these files that have changes"
 git diff
-sleep 20
+sleep 10
 
 # Check which files are being staged
+echo " "
+echo "Checking which files have to be staged"
 git status 
-sleep 20
+sleep 10
 
 # Git operations with wait intervals
+echo " "
+echo "Adding files to staged area"
 git add .
-sleep 20
+sleep 10
 
 # Get the current user's name
 USER_NAME=$(git config user.name)
@@ -53,12 +60,18 @@ CHANGED_FILES=$(git diff --name-only | head -n 10 | tr '\n' ' ')
 COMMIT_MESSAGE="Push by: $USER_NAME | Updated files: $CHANGED_FILES"
  
 # Commit changes with generated message
+echo " "
+echo "Commiting the changes"
 git commit -m "$COMMIT_MESSAGE"
-sleep 20
+sleep 10
 
 # pushing the changes onto main branch
+echo " "
+echo "Pushing all changes to remote repository"
 git push -u main
-sleep 20 
+sleep 10 
 
 # Check log of commit
+echo " "
+echo "Checking logs of the git"
 git log --oneline
