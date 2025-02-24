d78a45715e3120ab777200bb0b232d6ea5b598e5
surajB2024 <surajbhalerao2024@gmail.com>
2025-02-24 23:34:45 +0530

    Suraj: Changed some files

--- src/main/java/com/aepl/base/TestBase.java
+++ src/main/java/com/aepl/base/TestBase.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
@@ -31,7 +30,6 @@ public class TestBase {
 		driver = WebDriverFactory.getWebDriver(browserType);
 		driver.manage().window().maximize();
 		driver.get(ConfigProperties.getProperty("base.url"));
-		((JavascriptExecutor) driver).executeScript("document.body.style.zoom='75%'");
 		logger.info("Navigated to: " + ConfigProperties.getProperty("base.url"));
 	}
 
